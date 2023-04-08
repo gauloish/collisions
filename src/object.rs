3a8ebdf18bc9154c2a2884b6246610052e8c89c5
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-08 02:15:22 -0300

    added struct Object<N> documentation

--- src/object.rs
+++ src/object.rs
@@ -3,6 +3,14 @@ extern crate glium;
 use crate::geometry;
 use crate::settings;
 
+/// A renderizable sphere
+///
+/// * `sphere`: Object sphere
+/// * `center`: Sphere center
+/// * `vertices`: Vertex buffer
+/// * `indices`: Index buffer
+/// * `program`: Shader program
+/// * `color`: Index color
 pub struct Object<const N: usize> {
     sphere: geometry::Sphere<N>,
     center: geometry::Point,
