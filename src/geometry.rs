4b3a3840e73e93e94003a13fcba553ecd236d050
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-07 22:27:42 -0300

    added documentation

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,3 +1,8 @@
+/// Emulate a sphere in plane
+///
+/// * `points`: Sphere points
+/// * `center`: Sphere center
+/// * `radius`: Sphere radius
 pub struct Sphere<const N: usize> {
     pub points: [[f64; 2]; N],
     pub center: [f64; 2],
@@ -5,6 +10,10 @@ pub struct Sphere<const N: usize> {
 }
 
 impl<const N: usize> Sphere<N> {
+    /// Create a new sphere
+    ///
+    /// * `center`: Sphere center
+    /// * `radius`: Sphere radius
     pub fn new(center: &[f64; 2], radius: &f64) -> Sphere<N> {
         let points = std::array::from_fn::<_, N, _>(|index| {
             let angle = 2.0 * std::f64::consts::PI * (index as f64) / (N as f64);
