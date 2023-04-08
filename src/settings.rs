00a582b4c231a39ac8a037e0babee28da5099ee2
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-07 22:40:31 -0300

    added settings.rs

---
+++ src/settings.rs
@@ -0,0 +1,14 @@
+const width: u32 = 625;
+const height: u32 = 625;
+
+const title: String = String::from("Collisions");
+
+const colors: [[f64; 3]; 7] = [
+    [0.70, 0.8, 0.14],  // red
+    [0.09, 0.63, 0.69], // blue
+    [0.76, 0.65, 0.07], // yellow
+    [0.06, 0.72, 0.44], // green
+    [0.38, 0.04, 0.52], // purple
+    [0.70, 0.32, 0.04], // orange
+    [0.69, 0.05, 0.14], // pink
+];
