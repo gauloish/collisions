1026371b73a9a3e9800df1ac38e602e7d1a64323
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-08 03:17:32 -0300

    added and implemented Object::new() function

--- src/object.rs
+++ src/object.rs
@@ -4,13 +4,15 @@ use crate::geometry;
 use crate::settings;
 
 /// Generate vertices array to vertex buffer
-fn vertices_data<const N: usize>(object: &Object<N>) -> [geometry::Point; N + 1] {
+///
+/// * `sphere`: Sphere to get verices array
+fn vertices_data<const N: usize>(sphere: &geometry::Sphere<N>) -> [geometry::Point; N + 1] {
     let data: [geometry::Point; N + 1];
 
-    data[0] = object.center;
+    data[0] = sphere.center;
 
     for index in (0..N) {
-        data[index + 1] = object.sphere.points[index];
+        data[index + 1] = sphere.points[index];
     }
 
     data
@@ -39,11 +41,53 @@ fn indices_data() -> [u16; 3 * N] {
 /// * `color`: Index color
 pub struct Object<const N: usize> {
     sphere: geometry::Sphere<N>,
-    center: geometry::Point,
     vertices: glium::VertexBuffer,
     indices: glium::IndexBuffer,
     program: glium::Program,
     color: usize,
 }
 
-impl<const N: usize> Object<N> {}
+impl<const N: usize> Object<N> {
+    pub fn new(color: usize, radius: f64, display: &glium::Display) -> Object<N> {
+        let shape = vertices_data(&sphere);
+        let index = indices_data();
+
+        let vertex_shader = r#"
+            #version 140
+            in vec2 position;
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        "#;
+
+        let fragment_shader = r#"
+            #version 140
+            out vec4 color;
+            void main() {
+                color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        "#;
+
+        let center: geometry::Point = geometry::Point::from([0.0, 0.0]);
+        let sphere: geometry::Sphere<N> = geometry::Sphere::new(center, radius);
+
+        let program =
+            glium::Program::from_source(&display, vertex_shader, fragment_shader, None).unwrap();
+
+        let vertices = glium::VertexBuffer::new(&display, &shape).unwrap();
+        let indices = glium::IndexBuffer::new(
+            &display,
+            glium::index::PrimitiveType::TrianglesList,
+            &indices,
+        )
+        .unwrap();
+
+        Object::<N> {
+            sphere,
+            vertices,
+            indices,
+            program,
+            color,
+        }
+    }
+}
