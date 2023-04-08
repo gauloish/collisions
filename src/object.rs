ec894f3198960f85fefc980cd14d8ecc226ee7f7
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-08 02:12:22 -0300

    added struct Object<N>

--- src/object.rs
+++ src/object.rs
@@ -2,3 +2,12 @@ extern crate glium;
 
 use crate::geometry;
 use crate::settings;
+
+pub struct Object<const N: usize> {
+    sphere: geometry::Sphere<N>,
+    center: geometry::Point,
+    vertices: glium::VertexBuffer,
+    indices: glium::IndexBuffer,
+    program: glium::Program,
+    color: usize,
+}
