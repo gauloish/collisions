a26793ef03952a21f323fbac207ea7964e6a1c19
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-08 02:37:28 -0300

    implemented vertices_data and indices_data

--- src/object.rs
+++ src/object.rs
@@ -19,3 +19,29 @@ pub struct Object<const N: usize> {
     program: glium::Program,
     color: usize,
 }
+
+impl<const N: usize> Object<N> {
+    fn vertices_data(&self) -> [geometry::Point; N + 1] {
+        let data: [geometry::Point; N + 1];
+
+        data[0] = center;
+
+        for index in (0..N) {
+            data[index + 1] = self.sphere.points[index];
+        }
+
+        data
+    }
+
+    fn indices_data(&self) -> [u16; 3 * N] {
+        let data: [u16; 3 * N];
+
+        for index in (0..N) {
+            data[3 * index + 0] = 0;
+            data[3 * index + 1] = index;
+            data[3 * index + 2] = index + 1;
+        }
+
+        data
+    }
+}
