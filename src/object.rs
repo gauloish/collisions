cad825818cbfe4cf817f404292b68b33ab40ca8f
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-09 12:51:46 -0300

    added color in shader code (glsl)

--- src/object.rs
+++ src/object.rs
@@ -68,17 +68,27 @@ impl Object {
 
         let vertex_shader = r#"
             #version 140
+
             in vec2 position;
+            in vec3 color;
+
+            out vec3 _color;
+
             void main() {
                 gl_Position = vec4(position, 0.0, 1.0);
+                _color = color;
             }
         "#;
 
         let fragment_shader = r#"
             #version 140
+            
+            in vec3 _color;
+
             out vec4 color;
+
             void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
+                color = vec4(_color, 0.0);
             }
         "#;
 
