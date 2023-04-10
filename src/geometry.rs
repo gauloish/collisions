cd7ed679f064df7027b92b8d8016b3866890c0d3
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-04-10 01:01:58 -0300

    changed some struct and methods design

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,4 +1,5 @@
 use crate::settings;
+use rand;
 
 /// Emulate a point in plane
 ///
@@ -13,12 +14,14 @@ pub struct Point {
 /// Emulate a sphere in plane
 ///
 /// * `points`: Sphere points
-/// * `center`: Sphere center
+/// * `center`: Sphere center (using center as uniform in render)
+/// * `velocity`: Sphere velocity (using velocity as uniform in render)
 /// * `radius`: Sphere radius
 #[derive(Clone, Copy, Debug)]
 pub struct Sphere {
     pub points: [Point; settings::LENGTH],
-    pub center: Point,
+    pub center: [f64; 2],
+    pub velocity: [f64; 2],
     pub radius: f64,
 }
 
@@ -26,11 +29,11 @@ impl Point {
     /// Create a point from array
     ///
     /// * `color`: Color index
-    pub fn from(color: usize) -> Point {
-        Point {
-            position: [0.0, 0.0],
-            color: settings::COLORS[color],
-        }
+    pub fn new(position: [f64; 2]) -> Point {
+        let index = rand::random::<usize>() % 7;
+        let color = settings::COLORS[index];
+
+        Point { position, color }
     }
 }
 
@@ -39,15 +42,16 @@ impl Sphere {
     ///
     /// * `center`: Sphere center
     /// * `radius`: Sphere radius
-    /// * `color`: Color index
-    pub fn new(center: Point, radius: f64, color: usize) -> Sphere {
+    pub fn new(center: [f64; 2], radius: f64) -> Sphere {
         let points: [Point; settings::LENGTH] = std::array::from_fn(|index| {
             let angle = 2.0 * std::f64::consts::PI * (index as f64) / (settings::LENGTH as f64);
 
             let position: [f64; 2] = [
-                center.position[0] + radius * angle.cos(),
-                center.position[1] + radius * angle.sin(),
+                center[0] + radius * angle.cos(),
+                center[1] + radius * angle.sin(),
             ];
+
+            let color = rand::random::<usize>() % 7;
 
             Point {
                 position,
@@ -55,9 +59,18 @@ impl Sphere {
             }
         });
 
+        let angle = rand::random::<f64>();
+        let multiplier = 1 + rand::random::<usize>() % 100;
+
+        let velocity: [f64; 2] = [
+            ((multiplier as f64) / 100.0) * radius * angle.cos(),
+            ((multiplier as f64) / 100.0) * radius * angle.sin(),
+        ];
+
         Sphere {
             points,
             center,
+            velocity,
             radius,
         }
     }
